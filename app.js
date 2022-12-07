18bbd64a5330f38fc566fa9fe5e284fdfda11aed
anil <mulakalapallianil>
2022-12-07 07:53:37 +0000

    gadgets page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/gadgets", (request, response) => {
-  response.send("./gadgets.html"{ root: __dirname });
+  response.sendFile("./gadgets.html", { root: __dirname });
 });
+
 module.exports = app;
